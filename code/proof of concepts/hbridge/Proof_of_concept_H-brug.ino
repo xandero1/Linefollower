51dddaf785a843c2ca92f6a6d7cf661222fd08ee
xandero1 <116888860+xandero1@users.noreply.github.com>
2022-12-10 20:13:49 +0100

    Add files via upload

---
+++ code/proof of concepts/hbridge/Proof_of_concept_H-brug.ino
@@ -0,0 +1,104 @@
+                              // Motor Rechts
+const int IN1 = 3;
+const int IN2 = 9;
+                              // Motor Links
+const int IN3 = 10;
+const int IN4 = 11;
+
+bool StatusKnop;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(IN1, OUTPUT); 
+  pinMode(IN2, OUTPUT); 
+  pinMode(IN3, OUTPUT); 
+  pinMode(IN4, OUTPUT);
+}
+
+
+
+
+void loop() {
+    // Wagentje voorwaarts
+    for (int i = 0; i <= 255; i++)
+    {
+      analogWrite(IN1, i);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, i);
+      delay(10);
+    }
+    for (int i = 255; i >= 0; i--)
+    {
+      analogWrite(IN1, i);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, i);
+      delay(10);
+    }
+    delay(2000);
+
+
+
+    // Wagentje achterwaarts
+    for (int i = 0; i <= 255; i++)
+    {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, i);
+      analogWrite(IN3, i);
+      analogWrite(IN4, 0);
+      delay(10);
+    }
+    for (int i = 255; i >= 0; i--)
+    {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, i);
+      analogWrite(IN3, i);
+      analogWrite(IN4, 0);
+      delay(10);
+    }
+    delay(2000);
+
+
+
+    
+    // Wagentje rechts draaien
+    for (int i = 0; i <= 255; i++)
+    {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, i);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, i);
+      delay(10);
+    }
+    for (int i = 255; i >= 0; i--)
+    {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, i);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, i);
+      delay(10);
+    }
+    
+
+
+
+    // Wagentje links draaien
+    for (int i = 0; i <= 255; i++)
+    {
+      analogWrite(IN1, i);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, i);
+      analogWrite(IN4, 0);
+      delay(10);
+    }
+    for (int i = 255; i >= 0; i--)
+    {
+      analogWrite(IN1, i);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, i);
+      analogWrite(IN4, 0);
+      delay(10);
+    }
+    delay(2000);    
+}
