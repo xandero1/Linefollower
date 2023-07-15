c12b351ba53c712db11500c58909365499a6ef52
xandero1 <116888860+xandero1@users.noreply.github.com>
2023-07-15 14:37:22 +0200

    Add files via upload

--- code/proof of concepts/hbridge/Proof_of_concept_H-brug.ino
+++ code/proof of concepts/hbridge/Proof_of_concept_H-brug.ino
@@ -1,9 +1,9 @@
                               // Motor Rechts
-const int IN1 = 3;
-const int IN2 = 9;
+const int IN1 = 6;
+const int IN2 = 5;
                               // Motor Links
-const int IN3 = 10;
-const int IN4 = 11;
+const int IN3 = 11;
+const int IN4 = 10;
 
 bool StatusKnop;
 
@@ -65,18 +65,18 @@ void loop() {
     // Wagentje rechts draaien
     for (int i = 0; i <= 255; i++)
     {
-      analogWrite(IN1, 0);
-      analogWrite(IN2, i);
-      analogWrite(IN3, 0);
-      analogWrite(IN4, i);
+      analogWrite(IN1, i);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, i);
+      analogWrite(IN4, 0);
       delay(10);
     }
     for (int i = 255; i >= 0; i--)
     {
-      analogWrite(IN1, 0);
-      analogWrite(IN2, i);
-      analogWrite(IN3, 0);
-      analogWrite(IN4, i);
+      analogWrite(IN1, i);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, i);
+      analogWrite(IN4, 0);
       delay(10);
     }
     
@@ -86,18 +86,18 @@ void loop() {
     // Wagentje links draaien
     for (int i = 0; i <= 255; i++)
     {
-      analogWrite(IN1, i);
-      analogWrite(IN2, 0);
-      analogWrite(IN3, i);
-      analogWrite(IN4, 0);
+      analogWrite(IN1, 0);
+      analogWrite(IN2, i);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, i);
       delay(10);
     }
     for (int i = 255; i >= 0; i--)
     {
-      analogWrite(IN1, i);
-      analogWrite(IN2, 0);
-      analogWrite(IN3, i);
-      analogWrite(IN4, 0);
+      analogWrite(IN1, 0);
+      analogWrite(IN2, i);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, i);
       delay(10);
     }
     delay(2000);    
